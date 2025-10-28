f0762e2040308c1922561ec216ebb70ade708dd1
Rubel <rubel.mozumder@outlook.com>
2025-10-28 10:49:52 +0100

    North tool is properly added but image is not found in nomad.

--- src/north_entry_points.py
+++
@@ -1,9 +0,0 @@
-from nomad.config.models.north import NORTHTool
-from nomad.config.models.plugins import NorthToolEntryPoint
-
-tool = NORTHTool(
-    short_description='Jupyter Notebook server in NOMAD NORTH.',
-    image='ghcr.io/rubelmozumder/test-north/jupyter:pr-1',
-)
-
-north_tool = NorthToolEntryPoint(id='jupyter', north_tool=tool)
