343462fb8e93aead61ccea25e5c861cf6128b814
Rubel <rubel.mozumder@outlook.com>
2025-11-20 13:57:38 +0100

    add xps image for test.

--- src/test_north/north_entry_points.py
+++ src/test_north/north_entry_points.py
@@ -3,16 +3,14 @@ from nomad.config.models.plugins import NorthToolEntryPoint
 
 tool = NORTHTool(
     short_description='Jupyter Notebook server in NOMAD NORTH.',
-    image='ghcr.io/rubelmozumder/test-north/jupyter:latest',
+    image='gitlab-registry.mpcdf.mpg.de/nomad-lab/north/xps:master',
     description='Test SPM: Scanning Probe Microscopy Jupyter Notebook',
     external_mounts=[],
     file_extensions=['ipynb', 'nxs', 'h5', 'hdf5'],
     icon='logo/jupyter.svg',
     image_pull_policy='Always',
     default_url='/lab',
-    maintainer=[
-        {'email': 'rubel.mozumder@physik.hu-berlin.de', 'name': 'Rubel Mozumder'}
-    ],
+    maintainer=[{'email': 'rubel.mozumder@physik.hu-berlin.de', 'name': 'Rubel Mozumder'}],
     mount_path='/home/jovyan',
     path_prefix='lab/tree',
     privileged=False,
